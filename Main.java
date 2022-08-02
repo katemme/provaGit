dc9ae7b7317bddace3c909fd463edfe554dbe717
Caterina Meneghini <caterina.meneghini@edu.unife.it>
2022-08-02 17:55:11 +0200

    ho inserito una stampa nel main

--- Main.java
+++ Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        
+        System.out.println("Stampa prima riga");
+        System.out.println("Stampa seconda riga");
     }
 }
